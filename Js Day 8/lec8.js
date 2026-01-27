d1f81f1aa0011e410b11cae61dbc5ea5591f6bf2
Anushka-Tripathi <anushka.official1911@gmail.com>
2026-01-27 12:33:57 +0530

    Js Day 8

---
+++ Js Day 8/lec8.js
@@ -0,0 +1,48 @@
+var a = 10;
+console.log(a);
+
+function demo() {
+    let b = 20;
+    console.log(b);
+}
+demo();
+
+// Call Stack Example
+function firstFunction() {
+    console.log("Inside First Function");
+    secondFunction();
+    console.log("Exiting First Function");
+}
+function secondFunction() {
+    console.log("Inside Second Function");
+    thirdFunction();
+    console.log("Exiting Second Function");
+}
+function thirdFunction() {
+    console.log("Inside Third Function");
+}
+firstFunction();
+// Output:
+// Inside First Function
+// Inside Second Function
+// Inside Third Function
+// Exiting Second Function
+// Exiting First Function
+// The call stack helps in managing function calls and their execution contexts.
+// Note: The above code demonstrates the concept of execution context and call stack in JavaScript.
+// The global execution context is created when the script starts executing.
+// Each function call creates a new execution context that is pushed onto the call stack.
+// When the function execution is complete, its context is popped off the stack.
+// The call stack follows the Last In First Out (LIFO) principle.
+
+// Object Methods
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+console.log(person.fullName()); // John Doe
+console.log(person.valueOf()); // [object Object]
+console.log(person.entries()); // [ [ 'firstName', 'John' ], [ 'lastName', 'Doe' ], [ 'fullName', [Function: fullName] ] ]
