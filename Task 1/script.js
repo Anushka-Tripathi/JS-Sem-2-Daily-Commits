98cdedc89a61dabbdd0f037a89bcefafc47fdf26
Anushka-Tripathi <anushka.official1911@gmail.com>
2026-02-03 12:34:49 +0530

    Js Day 11

--- Task 1/script.js
+++ Task 1/script.js
@@ -14,3 +14,34 @@ for (let i = 0; i < highlight.length; i++) {
     console.log(`Total important elements: ${highlight.length}`);
 }
 
+// Task 3: Paragraph Analyser(TagName)
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs);
+for (let i = 0; i < paragraphs.length; i++) {
+    if (i % 2 === 0) {
+        paragraphs[i].style.color = '#fa282c'; // Even paragraphs
+    } else {
+        paragraphs[i].style.color = '#b79df4'; // Odd paragraphs
+    }
+}
+paragraphs[paragraphs.length - 1].style.fontWeight = 'bold'; // Last paragraph
+
+// Task 4: First Match Modifier(QuerySelector) - change background color and change text to i am first box
+const firstNote = document.querySelector('.note');
+firstNote.style.backgroundColor = '#f7daad';
+firstNote.style.padding = '10px';
+firstNote.style.borderLeft = '5px solid #5bc0de';
+firstNote.textContent = 'I am the first box';
+
+// Task 5: All Matches Modifier(QuerySelectorAll)
+const section = document.getElementById('task5-section');
+const allNotes = section.querySelectorAll('p');
+
+for(let i = 0; i < allNotes.length; i++) {
+    allNotes[i].style.color = '#fab3e7';
+}
+
+// Task 6: Advanced Selection - innerText and innerHTML
+const advancedSection = document.getElementById('guest');
+console.log(advancedSection.innerText); // Logs text content without HTML tags
+console.log(advancedSection.innerHTML); // Logs HTML content including tags
